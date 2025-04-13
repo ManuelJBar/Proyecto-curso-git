a43cb8fb52cf020f962458185aa3980a2d792cba
ManuelBar21 <manuel.j.barrachina@gmail.com>
2025-04-14 01:12:52 +0200

    Añadir Ejercicios para Saber si lo puedo Hacer

---
+++ Nuevo Intento Seguir Curso/Ejercicio2.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int edad = 25;
+        double peso = 70.5;
+        char letra = 'A';
+        boolean esEstudiante = true;
+        String nombre = "Juan";
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Peso: " + peso);
+        System.out.println("Inicial: " + letra);
+        System.out.println("Estudiante: " + esEstudiante);
+    }
+}
